74bdf69c7ea813c174a00e2cb8e5a9a614dc09a3
Aarif Shaikh <xmohdaarifx@gmail.com>
2021-07-31 23:15:46 +0900

    update submissions (#4)

---
+++ Leetcode/Python/89_Gray_Code.py
@@ -0,0 +1,25 @@
+"""
+89. Gray Code
+An n bit gray code is a sequence of binary numbers differing by a single bit
+It has 2**n entries starting from 0.
+The first and the last entry also differ by a single bit
+
+Difficulty:- Medium
+
+https://leetcode.com/problems/gray-code/
+Input: n = 2
+Output: [0, 1, 3, 2]
+
+Necessary theory:-
+^ = Bitwise XOR operator
+>> = Bitwise right shift operator
+author:- aarifsk
+"""
+
+
+class Solution:
+    def gray_code(self, n: int) -> List[int]:
+        answer = []
+        for idx in range(2**n):
+            answer.append(idx ^ (idx >> 1))
+        return answer
